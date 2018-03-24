a204cb7d924757daf5fb3c61c779f376f7159396
Shuai-Hsun <victory2152@livemail.tw>
2018-03-24 15:24:06 -0700

    first commit

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 # print a nice greeting.
-def say_hello(username = "Lee"):
+def say_hello(username = "Vic"):
     return '<p>Hello %s!</p>\n' % username
 
 # some bits of text for the page.
