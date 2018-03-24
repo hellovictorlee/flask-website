cf98fd11c67082e6825308966619632656d3cc98
Shuai-Hsun <victory2152@livemail.tw>
2018-03-24 15:33:01 -0700

    first commit

--- application.py
+++ application.py
@@ -1,7 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+import os
+ 
+application = flask.Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+# Only enable Flask debugging if an env var is set to true
+application.debug = os.environ.get('FLASK_DEBUG') in ['true', 'True']
 
+# Get application version from env
+app_version = os.environ.get('APP_VERSION')
+
+# Get cool new feature flag from env
+enable_cool_new_feature = os.environ.get('ENABLE_COOL_NEW_FEATURE') in ['true', 'True']
+
+@application.route('/')
+def hello_world():
+    message = "Hello, world!"
+    return flask.render_template('index.html',
+                                  title=message,
+                                  flask_debug=application.debug,
+                                  app_version=app_version,
+                                  enable_cool_new_feature=enable_cool_new_feature)
+ 
+if __name__ == '__main__':
+    application.run(host='0.0.0.0')
