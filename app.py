55e2496a27f346ec87ab106da8424d71ab7896d0
Shuai-Hsun <victory2152@livemail.tw>
2018-03-25 16:02:41 -0700

    modify: application.py, contact.html

--- app.py
+++ app.py
@@ -4,31 +4,31 @@ import json
 import os
  
 
-application = flask.Flask(__name__)
+app = flask.Flask(__name__)
 
 # Only enable Flask debugging if an env var is set to true
-application.debug = os.environ.get('FLASK_DEBUG') in ['true', 'True']
+app.debug = os.environ.get('FLASK_DEBUG') in ['true', 'True']
 
 # Get application version from env
-application_version = os.environ.get('APP_VERSION')
+#app = os.environ.get('APP_VERSION')
 
 # Get cool new feature flag from env
 enable_cool_new_feature = os.environ.get('ENABLE_COOL_NEW_FEATURE') in ['true', 'True']
 
 
-@application.route('/')
+@app.route('/')
 def index():
     return flask.render_template('index.html')
 
-@application.route('/about')
+@app.route('/about')
 def about():
     return flask.render_template('about.html')
 
-@application.route('/post')
+@app.route('/post')
 def post():
     return flask.render_template('post.html')
 
-@application.route('/contact', methods=['GET', 'POST'])
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if flask.request.method == 'POST':
         return flask.render_template('index.html')
@@ -36,4 +36,4 @@ def contact():
         return flask.render_template('contact.html')
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
