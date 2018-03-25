07e6df5aa2e85ac1dbd6575e8c62cfba4fcde538
Shuai-Hsun <victory2152@livemail.tw>
2018-03-24 18:26:36 -0700

    first commit

--- application.py
+++ application.py
@@ -14,7 +14,7 @@ enable_cool_new_feature = os.environ.get('ENABLE_COOL_NEW_FEATURE') in ['true', 
 
 @application.route('/')
 def hello_world():
-    message = "Hello, world!"
+    message = "Hello, Victor!"
     return flask.render_template('index.html',
                                   title=message,
                                   flask_debug=application.debug,
