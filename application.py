fb7a0ff535c54edef5e783bdf5eddba8204097bb
Shuai-Hsun <victory2152@livemail.tw>
2018-03-26 23:11:50 -0700

    basic version

--- application.py
+++ application.py
@@ -1,34 +1,33 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
+application = Flask(__name__)
 
-def create_app(config_name):
-    application = Flask(__name__, instance_relative_config=True)
+@application.route('/')
+def index():
+    return render_template('index.html')
 
-    @application.route('/')
-    def index():
-        return render_template('index.html')
+@application.route('/about')
+def about():
+    return render_template('about.html')
 
-    @application.route('/about')
-    def about():
-        return render_template('about.html')
-
-    @application.route('/post')
-    @application.route('/post/<page>')
-    def post(page=''):
-        try:
-            if any(page):
-                return render_template('post/' + page + '.html')
-            else:
-                return render_template('post.html')
-        except Exception:
-            return "error!!"
-
-    @application.route('/contact', methods=['GET', 'POST'])
-    def contact():
-        if request.method == 'POST':
-            return render_template('index.html')
+@application.route('/post')
+@application.route('/post/<page>')
+def post(page=''):
+    try:
+        if any(page):
+            return render_template('post/' + page + '.html')
         else:
-            return render_template('contact.html')
+            return render_template('post.html')
+    except Exception:
+        return "error!!"
 
-    return application
+@application.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('index.html')
+    else:
+        return render_template('contact.html')
+
+if __name__ == '__main__':
+    application.run()
