f3730a91975f904f6326a1296977c5b06b005758
Shuai-Hsun <hellovictorlee@gmail.com>
2018-06-04 19:49:45 -0700

    modify: map & canvas

---
+++ project/static/data/path_creator.py
@@ -0,0 +1,36 @@
+from PIL import Image
+import copy
+import json
+
+
+img = Image.open("../img/path_usamap.png")
+data = img.getdata()
+x = data.size[0]
+y = data.size[1]
+
+
+# make a list color for each days
+dic = {}
+state = ['0x00', '0x7f', '0xff']
+num = 0
+for i in state:
+    for j in state:
+        for k in state:
+            dic[(int(i, 0), int(j, 0), int(k, 0))] = num
+            num += 1
+# Get rid of common color - white & black
+del dic[(0, 0, 0)]
+del dic[(255, 255, 255)]
+
+
+mapper = {}
+for j in range(y):
+    for i in range(x):
+        for k in dic:
+            if (data[j*x+i][0], data[j*x+i][1], data[j*x+i][2]) == k:
+                mapper[str([i, j])] = dic[k]
+
+
+data = json.dumps(mapper)
+with open('data.json', 'w') as output:
+    output.write(data)
--- project/static/js/usamap.js
+++ project/static/js/usamap.js
@@ -6,36 +6,35 @@ var context = mapcanvas.getContext('2d');
 init();
 
 // cursor location
-var c_loc = [window.innerWidth/2, window.innerHeight/2];
-var c_r = 10;
+var c_loc = [-1, -1];
 
 function init() {
     resize();
+    getPath();
     window.addEventListener('mousemove', getMousePos, false);
-    setInterval(draw1, 25);
+    // setInterval(draw, 25);
 }
 
 function resize() {
     var w = document.getElementById("map").offsetWidth;
-    mapcanvas.width = w;
-    mapcanvas.height = w * 1002 / 1600;
+    mapcanvas.width = Math.round(w);
+    mapcanvas.height = Math.round(w * 1002 / 1600);
 }
 
 function getMousePos(e) {
     var event = e || window.event;
     var rect = mapcanvas.getBoundingClientRect();
-    c_loc[0] = event.clientX - rect.left;
-    c_loc[1] = event.clientY - rect.top;
+    var w = Math.round(event.clientX - rect.left);
+    var h = Math.round(event.clientY - rect.top);
+
+    if (0 <= w && w < mapcanvas.width && 0 <= h && h < mapcanvas.height)
+        c_loc = [w, h];
+    else
+        c_loc = [-1, -1];
 }
 
-function draw1(e) {
-    context.fillStyle = '#FF5733';
-    circle(c_loc[0], c_loc[1], 10);
-}
-
-function circle(x, y, r) {
-    context.beginPath();
-    context.arc(x, y, r, 0, 2*Math.PI, false);
-    context.closePath();
-    context.fill();
+function getPath() {
+    $.getJSON('../static/data/data.json', function(data) {         
+        console.log(data);
+    });
 }
