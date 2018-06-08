6e26ead0563eae3f78e67c2212821b604c216239
Shuai-Hsun <hellovictorlee@gmail.com>
2018-06-08 01:29:27 -0700

    add: key press control day path

--- project/static/js/usamap.js
+++ project/static/js/usamap.js
@@ -1,7 +1,7 @@
 console.log("USA Map");
 
 var mapcanvas = document.getElementById('mapcanvas');
-var context = mapcanvas.getContext('2d');
+var mapcontext = mapcanvas.getContext('2d');
 
 // cursor location
 var c_loc = [-1, -1];
@@ -26,7 +26,6 @@ function init() {
     window.addEventListener('mousemove', getMousePos, false);
     window.addEventListener('mousedown', daySelect, false);
     window.addEventListener('keypress', dayChange, false);
-    // setInterval(draw, 10);
 }
 
 function resize() {
@@ -49,17 +48,8 @@ function getMousePos(e) {
 
 function daySelect(e) {
     $("#mapcanvas").on('touchstart mousedown', function() {
-        // hide all days
-        for (i=1; i<24; i++) {
-            var s = "#day" + i.toString();
-            $(s).hide();
-        }
-
-        // show day of current section
-        if (getSection() != -1) {
-            var s = "#day" + getSection().toString();
-            $(s).show();
-        }
+        hide_all_days();
+        show_the_day_click();
     });
 }
 
@@ -72,10 +62,13 @@ function dayChange(e) {
         return;
     day = day >= 0 ? day % 24 : 0;
     draw();
+
+    hide_all_days();
+    show_the_day_keypress();
 }
 
 function draw() {
-    // context.clearRect(0, 0, mapcanvas.width, mapcanvas.height);
+    mapcontext.clearRect(0, 0, mapcanvas.width, mapcanvas.height);
 
     var current_section = getSection();
     var color_array = ["#aed6f1","#a9cce3","#d2b4de","#d7bde2","#f5b7b1","#e6b0aa","#a3e4d7","#a3e4d7","#a3e4d7","#abebc6","#f9e79f","#fad7a0","#f5cba7","#edbb99","#f7f9f9","#e5e7e9","#d5dbdb","#ccd1d1","#aeb6bf","#abb2b9","#e6b0aa","#f5b7b1","#d7bde2","#d2b4de"];
@@ -83,12 +76,34 @@ function draw() {
     var x, y;
 
     for (var key in dic) {
-        context.fillStyle = color_array[dic[key]];
-        x = Math.round(parseInt(key.split(" ")[0]) * scale);
-        y = Math.round(parseInt(key.split(" ")[1]) * scale);
+        if (dic[key] <= day) {
+            mapcontext.fillStyle = color_array[dic[key]];
+            x = Math.round(parseInt(key.split(" ")[0]) * scale);
+            y = Math.round(parseInt(key.split(" ")[1]) * scale);
 
-        circle(x, y, radius);
+            circle(x, y, radius);
+        }
     }
+}
+
+function hide_all_days() {
+    for (i=1; i<24; i++) {
+        var s = "#day" + i.toString();
+        $(s).hide();
+    }
+}
+
+function show_the_day_click() {
+    var section = getSection();
+    if (section != -1 && section <= day) {
+        var s = "#day" + getSection().toString();
+        $(s).show();
+    }
+}
+
+function show_the_day_keypress() {
+    var s = "#day" + day.toString();
+    $(s).show();
 }
 
 function getSection() {
@@ -108,10 +123,10 @@ function getSection() {
 }
 
 function circle(x, y, r) {
-    context.beginPath();
-    context.arc(x, y, r, 0, 2*Math.PI, false);
-    context.closePath();
-    context.fill();
+    mapcontext.beginPath();
+    mapcontext.arc(x, y, r, 0, 2*Math.PI, false);
+    mapcontext.closePath();
+    mapcontext.fill();
 }
 
 function distance(x1, y1, x2, y2) {
